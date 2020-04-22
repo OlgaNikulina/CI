ddcc0436532f003349cd1e72c40ddc52083e9d23
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-23 00:34:13 +0500

    added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldCalculate100() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculate500() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 500;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 500;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculate1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculate0() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+}
